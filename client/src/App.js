e65f09c0b75237b121b447a95352377d32544eea
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2022-08-26 13:42:01 +0900

    working audiocontrols

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import RadioStations from './components/RadioStations';
 import AudioControls from './components/AudioControls';
 
 function App() {
-  const [playPauseBtn, setPlayPauseBtn] = useState(false);
+  const [playPauseBtn, setPlayPauseBtn] = useState(true);
   const [volume, setVolume] = useState(0.5);
   const [muteBtn, setMuteBtn] = useState(false);
   const [station, setStation] = useState("LoFiGirl");
@@ -13,11 +14,13 @@ function App() {
   const start = () => {
     setPlayPauseBtn(true);
     setPlayVideo(true);
+    console.log('video has been started');
   }
 
   const pause = () => {
     setPlayPauseBtn(false);
     setPlayVideo(false);
+    console.log('video has been paused');
   }
 
   const LoFiGirl = () => {
@@ -26,6 +29,7 @@ function App() {
     setStation("LoFiGirl");
     setVideo("https://www.youtube.com/watch?v=WDXPJWIgX-o");
     console.log("LoFiGirl");
+    start();
   }
 
   const CoffeeShopRadio = () => {
@@ -34,6 +38,7 @@ function App() {
     setStation("CoffeeShopRadio");
     setVideo("https://www.youtube.com/watch?v=-5KAN9_CzSA");
     console.log("CoffeeShopRadio");
+    start();
   }
 
   const ChillHopMusic = () => {
@@ -42,6 +47,7 @@ function App() {
     setStation("ChillHopMusic");
     setVideo("https://www.youtube.com/watch?v=7NOSDKb0HlU");
     console.log("ChillHopMusic");
+    start();
   }
 
   const ChilloutLounge = () => {
@@ -50,6 +56,7 @@ function App() {
     setStation("ChilloutLounge");
     setVideo("https://www.youtube.com/watch?v=9UMxZofMNbA");
     console.log("ChilloutLounge");
+    start();
   }
 
   const AnimeVibe = () => {
@@ -58,10 +65,17 @@ function App() {
     setStation("AnimeVibe");
     setVideo("https://www.youtube.com/watch?v=WDXPJWIgX-o");
     console.log("AnimeVibe");
+    start();
   }
 
   return (
-    <div className='interfaceContainer'>
+    <div className="App">
+      <ReactPlayer url={video}
+        volume={volume}
+        playing={playPauseBtn}
+        muted={muteBtn}
+      />
+    <div className='interfaceContwatch'>
       <div className='radioContainer'>
         <RadioStations
           LoFiGirl={LoFiGirl}
@@ -72,8 +86,17 @@ function App() {
         />
       </div>
       <div className='audioControlsContainer'>
-        <AudioControls />
+        <AudioControls
+          start={start}
+          pause={pause}
+          playPauseBtn={playPauseBtn}
+          muteBtn={muteBtn}
+          setMuteBtn={setMuteBtn}
+          volume={volume}
+          setVolume={setVolume}
+        />
       </div>
+    </div>
     </div>
   );
 }
